70789147fc09bf72b226747cbbafb37d27d9d400
TanyaDRO <drtanja@yandex.ru>
2022-02-08 11:16:31 +0300

    changed by comments

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -16,33 +16,24 @@ public class DataGenerator {
     public static String generateDate(int shift) {
 
         String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
         return date;
     }
 
     public static String generateCity(String locale) {
 
         String city = faker.address().cityName();
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
-        return city;
+        return city.replace('ё', 'е');
     }
 
     public static String generateName(String locale) {
 
         String name = faker.name().fullName();
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
-        return name;
+        return name.replace('ё', 'е');
     }
 
     public static String generatePhone(String locale) {
 
         String phone = faker.phoneNumber().phoneNumber();
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
         return phone;
     }
 
@@ -53,36 +44,14 @@ public class DataGenerator {
         public static UserInfo generateUser(String locale) {
 
             UserInfo user = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
-
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
             return user;
         }
     }
 
     @Value
-   public static class UserInfo {
+    public static class UserInfo {
         String city;
         String name;
         String phone;
-
-
-        public UserInfo(String city, String name, String phone) {
-            this.city = city.replace('ё', 'е');
-            this.name = name.replace('ё', 'е');
-            this.phone = phone.replace('ё', 'е');
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getPhone() {
-            return phone;
-        }
     }
 }
